a6e066a72c47dc26419681c084fd364c60d0833a
ShadePrima <aleksandrag1984@gmail.com>
2022-08-11 13:53:44 +0300

    first way css

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
+
+const Post = styled.View`
+  padding: 15px;
+  background-color: red;
+  height: 100px;
+  width: 100px;
+  border-radius: 30px;
+`;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textText}>Мое первое приложение на ангдроид!</Text>
+
+      <Post />
     </View>
   );
 }
@@ -13,8 +22,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textText: {
+    color: "red",
+    fontSize: 44,
   },
 });
